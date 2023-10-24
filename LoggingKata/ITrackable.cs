c1cf9b1e26a85c1f36ca89862b5d443eea7666be
b-juu <erichassett4@gmail.com>
2023-10-24 13:01:46 -0400

    init commit

--- LoggingKata/ITrackable.cs
+++ LoggingKata/ITrackable.cs
@@ -5,4 +5,9 @@
         string Name { get; set; }
         Point Location { get; set; }
     }
+    public class TacoBell : ITrackable
+    {
+        public string Name { get; set; }
+        public Point Location { get; set; }
+    }
 }
